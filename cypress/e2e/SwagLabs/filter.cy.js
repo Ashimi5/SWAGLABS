063d5035c1954e0c9577a4dfe7628005f6099894
Ashim <ashimpandey18@gmail.com>
2024-08-28 20:04:59 +0545

    Update: Remove unnecessary empty folder

--- cypress/e2e/SwagLabs/filter.cy.js
+++ cypress/e2e/SwagLabs/filter.cy.js
@@ -4,8 +4,8 @@ describe('Filter the products',()=>{
 
         cy.visit('https://www.saucedemo.com');
 
-        cy.url().should('contain','saucedemo.com');
-        cy.url().should('eq','https://www.saucedemo.com');
+        cy.url().should('contain','saucedemo.com');  // Assertion for URL
+        
         cy.get('[data-test="username"]').type('standard_user');
         cy.get('[data-test="password"]').type('secret_sauce');
         cy.get('[data-test="login-button"]').click();
